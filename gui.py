b9abf665ce68789b7e27e2869187fc60d57cdedd
Lukas Merkle <lukas.merkle@tum.de>
2020-04-12 13:30:23 +0200

    added ggreen mark for active caster

--- gui.py
+++ gui.py
@@ -6,12 +6,13 @@ class DummyDevice():
     def __init__(self,name):
         self.friendly_name=name
 class DummyStatus():
-    def __init__(self,vl):
+    def __init__(self,vl, active):
         self.volume_level=vl
+        self.is_active_input=active
 class DummyChromecast():
-    def __init__(self, name):
+    def __init__(self, name, active=False):
         self.device = DummyDevice(name)
-        self.status = DummyStatus(0.5)
+        self.status = DummyStatus(0.5, active)
 
     def wait(self):
         pass
@@ -20,11 +21,16 @@ class DummyChromecast():
         self.status.volume_level=v
 
 
-class CC_Slider_Frame(Frame):
-    def __init__(self, root, cc, relief="ridge", padding=2):
-        super().__init__(root, relief=relief, padding=padding)
+class CC_Slider_Frame(LabelFrame):
+    def __init__(self, root, cc, relief="solid", padding=0):
+
+        super().__init__(root, relief=relief, padding=padding, style="mystyle.TFrame", text="WD")
         self.cc = cc
         self.cc.wait()
+
+        if cc.status.is_active_input:
+            self.configure(style="mystyle.green.TFrame")
+
 
         ## Create children
         current_value = cc.status.volume_level
@@ -33,9 +39,12 @@ class CC_Slider_Frame(Frame):
         ttk_label=Label(self, text=cc.device.friendly_name)
         self.ttk_percentage=Label(self, text=str(round(current_value*100,2)) + " %")
 
+        # set frame name
+        self.configure(text="    " + cc.device.friendly_name + "    ")
+
         scale.bind("<ButtonRelease-1>", self.scale_callback)
         scale.pack(side=BOTTOM)
-        ttk_label.pack(side=TOP)
+        #ttk_label.pack(side=TOP)
         self.ttk_percentage.pack(side=TOP)
 
     def scale_callback(self, v):
@@ -44,35 +53,33 @@ class CC_Slider_Frame(Frame):
         self.ttk_percentage.configure(text=str(round(v.widget.get(), 2)) + " %")
 
 
-
 class Control_Chromecasts_Gui():
     def __init__(self, master):
         frame = Frame(master, width=768, height=576)
         frame.pack()
         self.master = master
-        # self.mainframe = frame
         self.chromecasts=[]
 
-        self.button = Button(
-            frame, text="Exit", command=frame.quit
-        )
-        self.button.pack(side=BOTTOM)
-        self.research = Button(
-            frame, text="Search for CCs", command=self.__get_chromecasts
-        )
-        self.research.pack(side=BOTTOM)
+        # Create styles
+        style = Style()
+        style.configure("mystyle.TFrame", bordercolor="black", highlightthickness=5)
+        style.configure("mystyle.green.TFrame", bordercolor="#00cc00", highlightthickness=20)
 
         self.Found_CCs=Label(frame, text="Found chromecasts:")
         self.Found_CCs.pack(side=TOP)
 
         # Setup the chomecasts sliders
-        # self.chromecasts = [DummyChromecast("Arbeitszimmer"), DummyChromecast("Küche"), DummyChromecast("Wohnung"), DummyChromecast("Wohnzimmer")]
-        self.__get_chromecasts()
+        self.chromecasts = [DummyChromecast("Arbeitszimmer", active=True), DummyChromecast("Küche"), DummyChromecast("Wohnung"), DummyChromecast("Wohnzimmer")]
+        # self.__get_chromecasts()
 
         self.slider_frames = [CC_Slider_Frame(self.master, cc) for cc in self.chromecasts]
         [sf.pack(side=LEFT) for sf in self.slider_frames]
 
-        # self.__create_slider()
+
+        self.button = Button(frame, text="Exit", command=frame.quit)
+        self.button.pack(side=BOTTOM)
+        self.research = Button(frame, text="Search for CCs", command=self.__get_chromecasts)
+        self.research.pack(side=BOTTOM)
 
 
     def __get_chromecasts(self):
@@ -80,43 +87,6 @@ class Control_Chromecasts_Gui():
         self.chromecasts = CC.chromecasts
         return CC.chromecasts
 
-    def __create_sliders(self):
-        [c.wait() for c in self.chromecasts]
-        self.frames = [self.__create_slider(c.device.friendly_name, c.status.volume_level) for c in self.chromecasts]
-        [sc["frame"].pack(side=LEFT) for sc in self.frames]
-
-
-    def __create_slider(self, label, current_value):
-        # scale = Scale(self.master, from_=100, to=0, length=300, width=20, resolution=1, label=label)
-        frame = Frame(self.mainframe,relief="ridge", padding=2)
-        scale = Scale(frame, from_=100, to=0, orient = "vertical", length = 300)
-        scale.set(current_value*100)
-        ttk_label=Label(frame, text=label)
-        ttk_percentage=Label(frame, text=str(round(current_value*100,2)) + " %")
-
-        scale.bind("<ButtonRelease-1>", self.scale_callback(label))
-        scale.pack(side=BOTTOM)
-        ttk_label.pack(side=TOP)
-        ttk_percentage.pack(side=TOP)
-
-        return {"label": label, "frame": frame, "ttk_label": ttk_label, "ttk_percentage": ttk_percentage}
-
-    def scale_callback(self, i):
-        return lambda v:self.scale_callback_level2(i,v)
-
-    def scale_callback_level2(self, i, v):
-        print(v.widget._name, v.widget.get())
-        print(i)
-        cc = next(c for c in self.chromecasts if c.device.friendly_name == i)
-        if cc:
-            cc.set_volume(float(v.widget.get()/100))
-            print("set volume on ", cc.device.friendly_name)
-            percentage_label = next(f for f in self.frames if f["label"] == i)["ttk_percentage"]
-            percentage_label.configure(text=str(round(v.widget.get(),2)) + " %")
-            print(0)
-
-    def say_hi(self):
-        print("hi there, everyone!")
 
 if __name__ == "__main__":
 
