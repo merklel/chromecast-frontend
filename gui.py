4e19cd143df0f5d77a6e78bf6b637ff7400ee5bd
Lukas Merkle <lukas.merkle@tum.de>
2020-04-12 16:00:32 +0200

    added auto update sliders

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter.ttk import *
 from control_chromecasts import Control_Chromecasts
+import time
+
+
+# Settings
+TIME_BETWEEN_UPDATES = 2 # Time between updates of the sliders: read from the chromecasts
 
 class DummyDevice():
     def __init__(self,name):
@@ -34,40 +39,57 @@ class CC_Slider_Frame(LabelFrame):
 
         ## Create children
         current_value = cc.status.volume_level
-        scale = Scale(self, from_=100, to=0, orient = "vertical", length = 300)
-        scale.set(current_value*100)
+        self.scale = Scale(self, from_=100, to=0, orient = "vertical", length = 300)
+        self.scale.set(current_value*100)
         ttk_label=Label(self, text=cc.device.friendly_name)
         self.ttk_percentage=Label(self, text=str(round(current_value*100,2)) + " %")
 
         # set frame name
         self.configure(text="    " + cc.device.friendly_name + "    ")
 
-        scale.bind("<ButtonRelease-1>", self.scale_callback)
-        scale.pack(side=BOTTOM)
+        self.scale.bind("<ButtonRelease-1>", self.scale_callback)
+        self.scale.pack(side=BOTTOM)
         #ttk_label.pack(side=TOP)
         self.ttk_percentage.pack(side=TOP)
+
+
 
     def scale_callback(self, v):
         self.cc.set_volume(float(v.widget.get() / 100))
         print("set volume on ", self.cc.device.friendly_name)
         self.ttk_percentage.configure(text=str(round(v.widget.get(), 2)) + " %")
 
+    def update_percent_label(self):
+        current_value = round(self.cc.status.volume_level,2)
+        self.ttk_percentage.configure(text=str(current_value) + " %")
+        self.scale.set(current_value*100)
+
 
 class Control_Chromecasts_Gui():
     def __init__(self, master):
         frame = Frame(master, width=768, height=576)
         frame.pack()
+        self.frame = frame
         self.master = master
         self.chromecasts=[]
+        self.slider_frames=[]
 
         # Create styles
         style = Style()
         style.configure("mystyle.TFrame", bordercolor="black", highlightthickness=5)
         style.configure("mystyle.green.TFrame", bordercolor="#00cc00", highlightthickness=20)
 
-        self.Found_CCs=Label(frame, text="Found chromecasts:")
-        self.Found_CCs.pack(side=TOP)
+        # self.button = Button(frame, text="Exit", command=None)
+        # self.button.pack(side=BOTTOM)
+        self.research = Button(master, text="Search for CCs", command=self.__get_chromecasts)
+        self.research.pack(side=BOTTOM)
+        self.research = Button(master, text="Manual update control", command=self.manual_update)
+        self.research.pack(side=BOTTOM)
 
+        self.load_label = Label(frame, text="Currently looking for chromecasts, be patient...")
+        self.load_label.pack()
+
+    def get_and_draw_chromecasts(self):
         # Setup the chomecasts sliders
         # self.chromecasts = [DummyChromecast("Arbeitszimmer", active=True), DummyChromecast("Küche"), DummyChromecast("Wohnung"), DummyChromecast("Wohnzimmer")]
         self.__get_chromecasts()
@@ -75,27 +97,48 @@ class Control_Chromecasts_Gui():
         self.slider_frames = [CC_Slider_Frame(self.master, cc) for cc in self.chromecasts]
         [sf.pack(side=LEFT) for sf in self.slider_frames]
 
-
-        self.button = Button(frame, text="Exit", command=frame.quit)
-        self.button.pack(side=BOTTOM)
-        self.research = Button(frame, text="Search for CCs", command=self.__get_chromecasts)
-        self.research.pack(side=BOTTOM)
-
+        self.Found_CCs=Label(self.frame, text="Found chromecasts:")
+        self.Found_CCs.pack(side=TOP)
+        self.load_label.destroy()
 
     def __get_chromecasts(self):
         CC = Control_Chromecasts()
         self.chromecasts = CC.chromecasts
         return CC.chromecasts
 
+    def manual_update(self):
+        # update ccs
+        [cc.wait() for cc in self.chromecasts]
+
+        # update percent label
+        [cf.update_percent_label() for cf in self.slider_frames]
+
+
 
 if __name__ == "__main__":
 
     root = Tk()
     root.style = Style()
+    root.title("Control my Chromecasts")
     root.style.theme_use("clam")
     app = Control_Chromecasts_Gui(root)
+    root.update_idletasks()
+    root.update()
 
-    root.mainloop()
+    app.get_and_draw_chromecasts()
+
+    s0=time.monotonic()
+    while True:
+        root.update_idletasks()
+        root.update()
+
+        # print(s0)
+        # print(time.monotonic())
+        if time.monotonic()-s0 > TIME_BETWEEN_UPDATES:
+            print("update")
+            app.manual_update()
+            s0=time.monotonic()
+
     root.destroy()  # optional; see description below
 
 
